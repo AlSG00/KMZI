d692670498962665fac70f99979b920c8b3ce821
AlSG00 <82202986+AlSG00@users.noreply.github.com>
2021-04-18 13:39:00 +0700

    checkpoint

--- Freq.Designer.cs
+++ Freq.Designer.cs
@@ -33,20 +33,21 @@
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.button4 = new System.Windows.Forms.Button();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.groupHill = new System.Windows.Forms.GroupBox();
-            this.keyBoxProcessed = new System.Windows.Forms.RichTextBox();
-            this.keyBox = new System.Windows.Forms.RichTextBox();
-            this.label3 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.RichTextBox();
             this.textBox1 = new System.Windows.Forms.RichTextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.button5 = new System.Windows.Forms.Button();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.groupHill.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
@@ -61,81 +62,47 @@
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
-            // radioButton2
-            // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(117, 11);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(108, 17);
-            this.radioButton2.TabIndex = 37;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Расшифрование";
-            this.radioButton2.UseVisualStyleBackColor = true;
-            // 
             // groupHill
             // 
-            this.groupHill.Controls.Add(this.keyBoxProcessed);
-            this.groupHill.Controls.Add(this.keyBox);
             this.groupHill.Controls.Add(this.label3);
+            this.groupHill.Controls.Add(this.label2);
+            this.groupHill.Controls.Add(this.button5);
+            this.groupHill.Controls.Add(this.button3);
+            this.groupHill.Controls.Add(this.textBox4);
+            this.groupHill.Controls.Add(this.textBox3);
             this.groupHill.Controls.Add(this.textBox2);
             this.groupHill.Controls.Add(this.textBox1);
             this.groupHill.Controls.Add(this.button1);
             this.groupHill.Controls.Add(this.label1);
             this.groupHill.Controls.Add(this.button2);
-            this.groupHill.Location = new System.Drawing.Point(12, 34);
+            this.groupHill.Location = new System.Drawing.Point(12, 12);
             this.groupHill.Name = "groupHill";
-            this.groupHill.Size = new System.Drawing.Size(539, 475);
+            this.groupHill.Size = new System.Drawing.Size(539, 507);
             this.groupHill.TabIndex = 35;
             this.groupHill.TabStop = false;
             // 
-            // keyBoxProcessed
-            // 
-            this.keyBoxProcessed.Location = new System.Drawing.Point(272, 82);
-            this.keyBoxProcessed.Name = "keyBoxProcessed";
-            this.keyBoxProcessed.ReadOnly = true;
-            this.keyBoxProcessed.Size = new System.Drawing.Size(260, 44);
-            this.keyBoxProcessed.TabIndex = 15;
-            this.keyBoxProcessed.Text = "";
-            // 
-            // keyBox
-            // 
-            this.keyBox.Location = new System.Drawing.Point(272, 34);
-            this.keyBox.Name = "keyBox";
-            this.keyBox.Size = new System.Drawing.Size(260, 44);
-            this.keyBox.TabIndex = 12;
-            this.keyBox.Text = "";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(269, 18);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(33, 13);
-            this.label3.TabIndex = 11;
-            this.label3.Text = "Ключ";
-            // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(6, 263);
+            this.textBox2.Location = new System.Drawing.Point(7, 336);
             this.textBox2.Name = "textBox2";
             this.textBox2.ReadOnly = true;
-            this.textBox2.Size = new System.Drawing.Size(526, 206);
+            this.textBox2.Size = new System.Drawing.Size(432, 155);
             this.textBox2.TabIndex = 9;
             this.textBox2.Text = "";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(6, 34);
+            this.textBox1.Location = new System.Drawing.Point(6, 42);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(260, 92);
+            this.textBox1.Size = new System.Drawing.Size(433, 145);
             this.textBox1.TabIndex = 8;
             this.textBox1.Text = "";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(272, 132);
+            this.button1.Location = new System.Drawing.Point(227, 193);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(260, 26);
+            this.button1.Size = new System.Drawing.Size(212, 26);
             this.button1.TabIndex = 6;
             this.button1.Text = "Очистить поля";
             this.button1.UseVisualStyleBackColor = true;
@@ -143,7 +110,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, 18);
+            this.label1.Location = new System.Drawing.Point(6, 17);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(37, 13);
             this.label1.TabIndex = 4;
@@ -151,23 +118,13 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(6, 132);
+            this.button2.Location = new System.Drawing.Point(7, 193);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(260, 26);
+            this.button2.Size = new System.Drawing.Size(212, 26);
             this.button2.TabIndex = 6;
+            this.button2.Text = "Обработать текст";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // radioButton1
-            // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(21, 11);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(90, 17);
-            this.radioButton1.TabIndex = 36;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Шифрование";
-            this.radioButton1.UseVisualStyleBackColor = true;
             // 
             // openFileDialog1
             // 
@@ -176,9 +133,9 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(596, 532);
+            this.button5.Location = new System.Drawing.Point(58, 10);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(260, 26);
+            this.button5.Size = new System.Drawing.Size(142, 26);
             this.button5.TabIndex = 16;
             this.button5.Text = "открыть файл";
             this.button5.UseVisualStyleBackColor = true;
@@ -190,7 +147,7 @@
             this.chart1.ChartAreas.Add(chartArea1);
             legend1.Name = "Legend1";
             this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(572, 34);
+            this.chart1.Location = new System.Drawing.Point(557, 22);
             this.chart1.Name = "chart1";
             series1.ChartArea = "ChartArea1";
             series1.Legend = "Legend1";
@@ -206,48 +163,97 @@
             series2.Name = "Sample";
             this.chart1.Series.Add(series1);
             this.chart1.Series.Add(series2);
-            this.chart1.Size = new System.Drawing.Size(1027, 475);
+            this.chart1.Size = new System.Drawing.Size(982, 497);
             this.chart1.TabIndex = 46;
             this.chart1.Text = "chart1";
             this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(94, 238);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(266, 20);
+            this.textBox3.TabIndex = 16;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(94, 264);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(266, 20);
+            this.textBox4.TabIndex = 17;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(94, 290);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(266, 26);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "Преобразовать";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 241);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Замена";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(64, 267);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 13);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "На:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(457, 54);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(87, 449);
+            this.richTextBox1.TabIndex = 21;
+            this.richTextBox1.Text = "";
             // 
             // Freq
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1642, 630);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.chart1);
-            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
-            this.Controls.Add(this.radioButton2);
             this.Controls.Add(this.groupHill);
-            this.Controls.Add(this.radioButton1);
             this.Name = "Freq";
             this.Text = "Частотный криптоанализ";
             this.groupHill.ResumeLayout(false);
             this.groupHill.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.RadioButton radioButton2;
         private System.Windows.Forms.GroupBox groupHill;
-        private System.Windows.Forms.RichTextBox keyBoxProcessed;
-        private System.Windows.Forms.RichTextBox keyBox;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.RichTextBox textBox2;
         private System.Windows.Forms.RichTextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
--- Freq.cs
+++ Freq.cs
@@ -22,13 +22,10 @@ namespace KMZI
         char[] rus = { 'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'э', 'ю', 'я', ' ' };
         char[] eng = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ' };
 
-
-        //Label[] labels = {/* l1, l2 */};
         char[] sample_rus = { ' ', 'о', 'е', 'а', 'и', 'н', 'т', 'с', 'р', 'в', 'л', 'к', 'м', 'д', 'п', 'у', 'я', 'ы', 'з', 'ъ', 'б', 'г', 'ч', 'й', 'х', 'ж', 'ю', 'ш', 'ц', 'щ', 'э', 'ф' };
         char[] sample_eng = { ' ', 'e', 't', 'a', 'o', 'n', 'i', 's', 'r', 'h', 'l', 'd', 'c', 'u', 'p', 'f', 'm', 'w', 'y', 'b', 'g', 'v', 'k', 'q', 'x', 'j', 'z' };
         float[] sample_rus_freq = { 0.175f, 0.09f, 0.072f, 0.062f, 0.062f, 0.053f, 0.053f, 0.045f, 0.04f, 0.038f, 0.035f, 0.028f, 0.026f, 0.025f, 0.023f, 0.021f, 0.018f, 0.016f, 0.016f, 0.014f, 0.014f, 0.013f, 0.012f, 0.01f, 0.009f, 0.007f, 0.006f, 0.006f, 0.004f, 0.003f, 0.003f, 0.002f };
         float[] sample_eng_freq = { 0.175f, 0.123f, 0.096f, 0.081f, 0.079f, 0.072f, 0.071f, 0.066f, 0.06f, 0.051f, 0.04f, 0.036f, 0.032f, 0.031f, 0.023f, 0.023f, 0.022f, 0.02f, 0.019f, 0.016f, 0.016f, 0.009f, 0.005f, 0.002f, 0.002f, 0.001f, 0.001f};
-
 
         char[] alphabet = null;
         char[] alphabet_sorted = null;
@@ -74,19 +71,23 @@ namespace KMZI
                 }
             }
 
-            textBox2.Text = textBox1.Text.ToLower();
+            textBox1.Text = textBox1.Text.ToLower();
 
-            //this.chart1.Series["Current"].Points.Clear();
-            //hystogram_generate_current(alphabet, alphabet_freq);
-
-            for (int i = 0; i < alphabet_sorted.Length; i++)
+            for (int i = 0; i < textBox1.TextLength; i++)
             {
-                //textBox2.Text = textBox2.Text.Replace(alphabet_sample[i], alphabet_sorted[i]);
-                textBox2.Text = textBox2.Text.Replace(alphabet_sorted[i], alphabet_sample[i]);
-
+                if (alphabet.Contains(textBox1.Text[i]))
+                {
+                    textBox2.Text += alphabet_sample[Array.IndexOf(alphabet_sorted, textBox1.Text[i])];
+                }
+                else
+                {
+                    textBox2.Text += textBox1.Text[i];
+                }
             }
-            //hystogram_generate_sample(alphabet, alphabet_sample, alphabet_sample_freq, "Sample");
+            alphabet_freq = calculate_frequency(textBox2.Text, alphabet);
 
+            this.chart1.Series["Current"].Points.Clear();
+            hystogram_generate_current(alphabet, alphabet_freq);
         }
 
         public void hystogram_generate_sample(char[] alphabet, char[] sample, float[] sample_freq, string series)
@@ -210,14 +211,19 @@ namespace KMZI
             }
         }
 
-        private void chart1_Click(object sender, EventArgs e)
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void button4_Click(object sender, EventArgs e)
+        private void chart1_Click(object sender, EventArgs e)
         {
-            this.Close();
+
         }
     }
 }
